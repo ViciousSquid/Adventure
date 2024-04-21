3330d39398790c78d4041d7d0fd8a5dfdd17076e
Rufus Pearce <161540961+ViciousSquid@users.noreply.github.com>
2024-04-21 01:56:58 +0100

    Add files via upload

--- adventures.py
+++ adventures.py
@@ -46,3 +46,23 @@ def get_room_image_url(adventure_name, room_name):
 def get_random_adventure():
     adventures = load_adventures()
     return random.choice(list(adventures.keys()))
+
+def get_cover_image_data(adventure_name):
+    adventure_path = load_adventures()[adventure_name]
+    with zipfile.ZipFile(adventure_path, 'r') as zip_ref:
+        try:
+            with zip_ref.open('cover.jpg', 'r') as file:
+                cover_image_data = file.read()
+                return cover_image_data
+        except KeyError:
+            return None
+
+def get_summary_text(adventure_name):
+    adventure_path = load_adventures()[adventure_name]
+    with zipfile.ZipFile(adventure_path, 'r') as zip_ref:
+        try:
+            with zip_ref.open('summary.txt', 'r') as file:
+                summary_text = file.read().decode('utf-8')
+                return summary_text
+        except KeyError:
+            return None
